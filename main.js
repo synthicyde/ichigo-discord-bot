fcc8fb4b1a1b1328b08903ab2e6619429079ba9f
synthicyde <buzzair1001@gmail.com>
2022-01-09 07:25:06 -0500

    Finally got intents working.

--- main.js
+++ main.js
@@ -3,12 +3,24 @@ const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const {myIntents} = require("./config.json");
+const client = new Client({ intents: myIntents });
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
 	console.log("Ichigo is awake!");
 });
 
-// Login to Discord with your client's token
+// Triggers for listen words
+const triggers = [
+	"seed",
+	"weezer",
+	"shaina",
+];
+
+client.on("messageCreate", message => {
+	if (message.content == "hello") message.reply("beep!");
+});
+
+// Login to Discord with your client"s token
 client.login(token);
