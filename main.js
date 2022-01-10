28bb361ee6ca3681afa9172b4265c278f8ba54ec
synthicyde <buzzair1001@gmail.com>
2022-01-10 06:36:20 -0500

    Moved activity to JSON file and made word detect case insensitive.

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ const { token } = require("./config.json");
 const {myIntents} = require("./intents.json");
 const client = new Client({ intents: myIntents });
 
-const randSong = [
-	"Weezer",
-	"Massive Attack",
-	"Zerwee",
-	"Minecraft",
-];
-
-const random = Math.floor(Math.random() * randSong.length);
+// Calls in the activity
+const activity = require("./activity.json"); // call JSON for activities
+let randomActivity = Math.floor(Math.random() * activity.length); // selects random activity
+let randomName = Math.floor(Math.random() * activity[randomActivity].name.length); // selects random name
+let activityOptions = activity[randomActivity].options; // selects to appropriate options
+// Return in console
+// console.log(activity[randomActivity].name[randomName]);
+// console.log(activityOptions);
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
 	console.log("Ichigo is awake!");
-	client.user.setActivity(randSong[random], { type: "LISTENING" });
+	client.user.setActivity(activity[randomActivity].name[randomName], { type: activityOptions });
 });
 
 // Triggers for listen words
@@ -29,7 +29,7 @@ const triggers = [
 ];
 
 client.on("messageCreate", message => {
-	if (triggers.some(word => message.content.includes(word))) {
+	if (triggers.some(word => message.content.toLowerCase().includes(word))) {
 		message.reply("beep!")
 	  };
 });
