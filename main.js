93236495e9bc9ba5c925c80083dcc23d12475bdb
synthicyde <buzzair1001@gmail.com>
2022-01-15 06:55:02 -0500

    Finalized random status, random message, and random at.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, Channel, Guild } = require("discord.js");
 const { token } = require("./config.json");
+// const SetRandomInterval = require("set-random-interval");
+
+// setRandomInterval code
+const setRandomInterval = (intervalFunction, minDelay, maxDelay) => {
+	let timeout;
+	const runInterval = () => {
+		const timeoutFunction = () => {
+			intervalFunction();
+			runInterval();
+		};
+		const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+		timeout = setTimeout(timeoutFunction, delay);
+	};
+	runInterval();
+	return {
+		clear() {
+			clearTimeout(timeout);
+		},
+	};
+};
 
 // Create a new client instance
-const {myIntents} = require("./intents.json");
+const { myIntents } = require("./intents.json");
 const client = new Client({ intents: myIntents });
 
 // Calls in the activity
@@ -21,13 +41,58 @@ client.once("ready", () => {
 	client.user.setActivity(activity[randomActivity].name[randomName], { type: activityOptions });
 });
 
+// Global needed
+const milliConv = 60000; // For converting minutes to milliseconds
+const channelrandom = "929655220009578539"; // CHANGE ID BEFORE RUN
+const shainaID = "<@205187968460259328>"; // CHANGE ID BEFORE RUN
+
+// Random Activity Change
+const mintimeactiv = milliConv * 15;
+const maxtimeactiv = milliConv * 60;
+const randNumber = () => {
+	return Math.random();
+};
+const newActivity = () => {
+	let newNumb = randNumber();
+	let newRandActivity = Math.floor(newNumb * activity.length);
+	let newRandName = Math.floor(newNumb * activity[newRandActivity].name.length);
+	client.user.setActivity(activity[newRandActivity].name[newRandName], { type: activity[newRandActivity].options });
+};
+
 // Triggers for listen words
 const triggers = require("./words.json");
 
+// Reply to trigger words
 client.on("messageCreate", message => {
 	if (triggers.some(word => message.content.toLowerCase().includes(word))) {
 		message.reply("beep!")
-	  };
+		// let channelreply = client.channels.cache.find(channel => channel.id == message.channelId);
+		// channelreply.send("beep!");
+	}
+});
+
+// Send random message
+const mintimebeep = milliConv * 15;
+const maxtimebeep = milliConv * 240;
+// const channelrandom = client.channels.cache.get("929655220009578539"); // CHANGE ID BEFORE RUN!!!
+console.log(channelrandom);
+function sendRandMessage() {
+	client.channels.cache.get(channelrandom).send("beep!");
+}
+
+// Random emoji to Shaina
+const minTimeShaina = milliConv * 60;
+const maxTimeShaina = milliConv * 24 * 60;
+// const shainaMessage = Guild.members.cache.find(user => user.id === "205187968460259328"); // CHANGE ID BEFORE RUN!!!
+const sendRandShaina = () => {
+	client.channels.cache.get(channelrandom).send(shainaID + " :heart:");
+};
+
+// All random functions
+client.on("ready", () => {
+	const randActSet = setRandomInterval(() => newActivity(), mintimeactiv, maxtimeactiv);
+	const beep = setRandomInterval(() => sendRandMessage(), mintimebeep, maxtimebeep);
+	const beep = setRandomInterval(() => sendRandShaina(), minTimeShaina, maxTimeShaina);
 });
 
 // Login to Discord with your client"s token
